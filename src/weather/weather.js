ca8ed0d6b5b1f8550e669cf0e59cc91071aabc9b
degastudio <degastudio@outlook.com>
2021-05-04 17:08:13 +0200

    read key catch

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -4,7 +4,14 @@ const fs = require('fs').promises;
 
 const getOpenWeatherConfigs = async (lang, units = 'metric') => {
     const file = path.join(path.dirname(require.main.path), '/.keys/open-weather.txt');
-    const key = await fs.readFile(file, { encoding: 'utf8' });
+
+    console.log('FILE PATH:', file);
+
+    const key = await fs.readFile(file, { encoding: 'utf8', flag: 'r' })
+        .catch((err) => {
+            console.log('FILE READING ERROR:', err);
+        });
+
     return `https://api.openweathermap.org/data/2.5/weather?appid=${key}&lang=${lang}&units=${units}&q=`;
 } 
 
